4a47dcb4ad125045533c5c1fab5bf35a6e4b3706
Michael Dreesen <mdreesen@Mac.lan>
2025-09-18 12:10:24 -0600

    feature(Using Tailwind, DaisyUI, and nuxt Icon)

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+
+  <button class="btn btn-outline btn-secondary">
+
+    View map
+    <Icon name="tabler:map" />
+  </button>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: ['@nuxt/eslint', '@nuxt/icon'],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  css: ["~/assets/css/main.css"],
 })
